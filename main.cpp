1441a6a12fda8ca4c816800c4c3be1d90438de0f
Théo Seigneuret-Poussard <108277467+Poussardtheo@users.noreply.github.com>
2023-04-04 11:20:05 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include "ppm_image.hpp"
+#include "hough_transform.hpp"
+
+using namespace std;
+
+int main() {
+string filename = "Info801/Projet/Theo2/m1projectcpp2.ppm";
+ppm_image image(filename);
+
+HoughTransform hough(360, sqrt(image.width()*image.width() + image.height()*image.height()), 150);
+hough.transform(image);
+
+vector<pair<double, double>> lines = hough.detect_lines();
+for (unsigned int i = 0; i < lines.size(); ++i) {
+    cout << "Ligne detectee : theta = " << lines[i].first << ", rho = " << lines[i].second << endl;
+}
+
+write_ppm_image("output.ppm", image.width(), image.height(), image.rgb_data());
+
+return 0;
+}
